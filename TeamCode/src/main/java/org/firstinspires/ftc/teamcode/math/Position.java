9ea5452f8767c8f1c3a9b7f95e0ff979a6600d43
bogdanbuzgaru <bogdan.buzgaru@gmail.com>
2026-02-18 14:19:59 +0200

    Far parameters added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Position.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Position.java
@@ -10,8 +10,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 public class Position {
     private Pose2D pose;
     private double heading;
-    private final double l = 12.8740157349;
-    private final double L = 14.4881889616;
+    private final double l = 12.8740157349;     //inch
+    private final double L = 14.4881889616;     //inch
     public Position (Pose2D pose){
         this.pose = pose;
     }
@@ -23,14 +23,14 @@ public class Position {
     private double distanceY(){
         double currentHeading = Math.abs(heading);
         if (currentHeading > 90){
-            currentHeading -= 90;
+            currentHeading -= 90;           //incadrare in patrat
         }
         return 2 * l * cos(currentHeading);
     }
     private double distanceX(){
         double currentHeading = Math.abs(heading);
         if (currentHeading > 90){
-            currentHeading -= 90;
+            currentHeading -= 90;           //incadrare in patrat
         }
         return L * cos(currentHeading + l * cos(90 - currentHeading));
     }
@@ -50,6 +50,7 @@ public class Position {
         double y = pose.getY(DistanceUnit.INCH);
         return y - distanceY() / 2;
     }
+    //-----------------------CLOSE----------------------
     private boolean isMaxXClose(){
         return (getMaxX() >= (144 - pose.getY(DistanceUnit.INCH)) && pose.getY(DistanceUnit.INCH) >= 72)
                 && pose.getX(DistanceUnit.INCH) <= 72;          //The centre of the robot is on the left side of the triangle
@@ -62,10 +63,26 @@ public class Position {
         return (getMaxY() >= 72 && pose.getX(DistanceUnit.INCH) >= 144 - getMaxY()
                 && pose.getX(DistanceUnit.INCH) <= getMaxY() );     //The centre of the robot is in the bottom of the triangle
     }
+    //-----------------------FAR----------------------
+    private boolean isMinYFar(){
+        return getMinY() <= 24 && pose.getX(DistanceUnit.INCH) >= 72 - (24 - getMinY())
+                && pose.getX(DistanceUnit.INCH) <= 72 + (24 - getMinY());       //Top of the far triangle
+    }
+    private boolean isMaxXFar(){
+        return getMaxX() >= 48 && pose.getY(DistanceUnit.INCH) <= 24
+                && getMaxX() - pose.getY(DistanceUnit.INCH) <= 48       //Left of the far triangle
+                && pose.getY(DistanceUnit.INCH) <= 72;
+    }
+    private boolean isMinXFar(){
+        return getMinX() <= 96 && pose.getY(DistanceUnit.INCH) <= 24
+                && getMinX() + pose.getY(DistanceUnit.INCH) <= 96       //Right of the far triangle
+                && pose.getY(DistanceUnit.INCH) >= 72;
+    }
+
     public boolean shootClose(){
         return isMaxXClose() || isMaxYClose() || isMinXClose();
     }
     public boolean shootHigh(){
-        return false;
+        return isMinYFar() || isMaxXFar() || isMinXFar();
     }
 }
