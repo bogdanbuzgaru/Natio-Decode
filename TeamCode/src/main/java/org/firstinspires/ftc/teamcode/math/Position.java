dd89a787844ba572beb9f7c4f779baca2940cdd7
bogdanbuzgaru <bogdan.buzgaru@gmail.com>
2026-02-18 13:53:15 +0200

    Close parameters added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Position.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Position.java
@@ -34,21 +34,38 @@ public class Position {
         }
         return L * cos(currentHeading + l * cos(90 - currentHeading));
     }
-
-    public double getMaxY(){
+    private double getMaxY(){
         double y = pose.getY(DistanceUnit.INCH);
         return y + distanceY() / 2;
     }
-    public double getMaxX(){
+    private double getMaxX(){
         double x = pose.getX(DistanceUnit.INCH);
         return x + distanceX() / 2;
     }
-    public double getMinX(){
+    private double getMinX(){
         double x = pose.getX(DistanceUnit.INCH);
         return x - distanceX() / 2;
     }
-    public double getMinY(){
+    private double getMinY(){
         double y = pose.getY(DistanceUnit.INCH);
         return y - distanceY() / 2;
+    }
+    private boolean isMaxXClose(){
+        return (getMaxX() >= (144 - pose.getY(DistanceUnit.INCH)) && pose.getY(DistanceUnit.INCH) >= 72)
+                && pose.getX(DistanceUnit.INCH) <= 72;          //The centre of the robot is on the left side of the triangle
+    }
+    private boolean isMinXClose(){
+        return (getMinX() >= 72 && pose.getY(DistanceUnit.INCH) >= 72)
+                && pose.getX(DistanceUnit.INCH) >= 72;          //The centre of the robot is on the right side of the triangle
+    }
+    private boolean isMaxYClose(){
+        return (getMaxY() >= 72 && pose.getX(DistanceUnit.INCH) >= 144 - getMaxY()
+                && pose.getX(DistanceUnit.INCH) <= getMaxY() );     //The centre of the robot is in the bottom of the triangle
+    }
+    public boolean shootClose(){
+        return isMaxXClose() || isMaxYClose() || isMinXClose();
+    }
+    public boolean shootHigh(){
+        return false;
     }
 }
